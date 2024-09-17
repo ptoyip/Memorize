049c96d48f6d48c93b5c30a8dcf419ed88a46068
ptoyip <potatoyip@icloud.com>
2024-09-17 16:12:38 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Yip Kwun Chuen on 2024/9/17.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
